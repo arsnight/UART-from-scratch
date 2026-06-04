f5389c539cb444bcd466d78dbf8c8a53dda27583
arsnight <adityars.ars1@gmail.com>
2026-06-04 12:10:11 +0530

    Parameterized baud gen

--- src/TX_baud_rate_gen.v
+++ src/TX_baud_rate_gen.v
@@ -1,19 +1,27 @@
-module baud_rate_gen(
+module baud_rate_gen #(
+    parameter BAUD_DIV = 868
+)(
     input clk,
+    input rst,
     output reg tick
 );
 
-reg [9:0] count = 0;
+localparam COUNT_WIDTH = $clog2(BAUD_DIV + 1);
+reg [COUNT_WIDTH-1:0] count = 0;
 
 always @(posedge clk) begin
-if (count == 10'd867) begin
-    tick <= 1;
-    count <= 0;
-end                                
-else begin
-    count <= count + 1'b1;
-    tick <= 0;
-end
+    tick <= 0; // initialise default value
+    if (rst) begin
+        count <= 0;
+    end else begin
+        if (count == BAUD_DIV - 1) begin
+            tick <= 1;
+            count <= 0;
+        end                                
+        else begin
+            count <= count + 1'b1;
+        end
+    end
 end
 
 endmodule
