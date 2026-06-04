d428aa425a944631e1062292ce1c99bbd154237e
arsnight <adityars.ars1@gmail.com>
2026-06-04 12:08:57 +0530

    Par

--- src/UART_TX.v
+++ src/UART_TX.v
@@ -1,43 +1,61 @@
 `timescale 1ns / 1ps
 
-module UART_TX(
+module UART_TX #(
+    parameter CLK_FREQ = 100_000_000, //Finally understood parameterized hierarchical design
+    parameter BAUD_RATE = 115200,
+    parameter BIT_COUNT = 8,
+    parameter NUM_STATES = 4 // For future scalability to add parity bits
+)(
     input clk,
-    input [7:0] parallel_in,
+    input rst, // Added reset port
+    input [BIT_COUNT-1:0] parallel_in,
     input start_tx,
-    output serial_out
+    output wire serial_out
     );
 
+    localparam BAUD_DIV = CLK_FREQ / BAUD_RATE;
 
-    reg [7:0] data_stored;
-    reg prev_state = 0;
-    reg edge_detect = 0;
-    reg edge_detect_d = 0;
+
+    reg [BIT_COUNT-1:0] data_stored = 0;
+    reg start_tx_prev = 0; //Changed three reg for edge detect to only one
+    wire edge_detect;
+    wire TX_busy;
+    
+    assign edge_detect = ~start_tx_prev & start_tx;
+    
     wire tick;
     
     always @(posedge clk) begin
-        edge_detect_d <= edge_detect;
-        if (prev_state == 0 && start_tx == 1) begin
-            data_stored <= parallel_in;
-            edge_detect <= 1;
-        end
-        
-        else if(edge_detect_d && tick) begin
-            edge_detect <= 0;
-        end
-                
-        prev_state <= start_tx;
+        if (rst) begin
+            start_tx_prev <= 0;
+            data_stored <= 0;
+        end else begin
+            start_tx_prev <= start_tx;
+            if (edge_detect && ~TX_busy) begin //Used TX_busy here
+                data_stored <= parallel_in;
+            end
+        end      
     end
       
-    baud_rate_gen baud_rate(
+    baud_rate_gen #(
+            .BAUD_DIV(BAUD_DIV)
+    )baud_gen(
         .clk(clk),
+        .rst(rst),
         .tick(tick)
         );
 
-    uart_tx_fsm fsm_init(
+    uart_tx_fsm #(
+        .BIT_COUNT(BIT_COUNT),
+        .NUM_STATES(NUM_STATES)
+    )
+    tx_fsm_inst(
         .clk(clk),
         .data_stored(data_stored),
         .tick(tick),
         .edge_detect(edge_detect),
+        .rst(rst),
+        .TX_busy(TX_busy),
         .serial_out(serial_out)
         );
             
