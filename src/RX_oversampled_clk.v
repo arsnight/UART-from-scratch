b6d4ef6087d9caa10609b4710ed5a9b2ead60ed0
arsnight <adityars.ars1@gmail.com>
2026-08-10 14:14:03 +0530

    Fixed Parameter propagation issue

--- src/RX_oversampled_clk.v
+++ src/RX_oversampled_clk.v
@@ -30,3 +30,44 @@ module oversampled_clk #(
         end    
     end
 endmodule
+
+module Oversampled_counter #(
+    parameter CLK_FREQ = 100_000_000,
+    parameter BAUD_RATE = 115200,
+    parameter OVERSAMPLING_RATE = 28
+)(
+    input clk,
+    input rst_trigger,
+    output reg [$clog2(OVERSAMPLING_RATE + 1) - 1:0] counter = 0
+);
+
+    wire tick;
+    localparam width = $clog2(OVERSAMPLING_RATE + 1); // Obtained the width of the parameter
+    localparam [width-1:0] Matched_rate = OVERSAMPLING_RATE - 1; //forced a 32 bit constant down to the required bit width
+    
+    oversampled_clk #(
+        .CLK_FREQ(CLK_FREQ),
+        .BAUD_RATE(BAUD_RATE),
+        .OVERSAMPLING_RATE(OVERSAMPLING_RATE)
+    )clk_tick(
+        .clk(clk),
+        .rst_trigger(rst_trigger),
+        .tick(tick)
+    );
+    
+    always @(posedge clk) begin
+    
+    
+        if (rst_trigger) begin  
+            counter <= 0;           
+        end
+        else if (tick) begin
+           if (counter == Matched_rate) begin
+                counter <= 0;
+           end
+           else begin
+                counter <= counter + 1'b1;
+           end
+        end
+    end    
+endmodule
